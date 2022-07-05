6416e8390d6f5558f530b1d35bee651b65f0cea9
Feredev <hector1567xd@gmail.com>
2022-07-05 02:02:34 -0400

    Borrando desperdicio de pruebas

--- src/client/DuckUs.java
+++ src/client/DuckUs.java
@@ -3,8 +3,6 @@ package client;
 import client.game.engine.GameContainer;
 import client.game.engine.GameNetwork;
 import client.game.engine.GameController;
-import client.game.nodes.Cuadrado;
-import client.game.nodes.Jugador;
 import common.DuckPacketReader;
 import common.game.engine.Scale;
 import common.networking.socket.SocketPublisher;
@@ -22,20 +20,16 @@ public class DuckUs {
 
         GameNetwork network = new GameNetwork(client);
 
-        Scale scale = new Scale(32, 1);
+        Scale scale = new Scale(32, 2);
         GameContainer container = new GameContainer(scale, network, controller);
 
         // GAME
 
-        /*Player player = new Player();
-        controller.addNode(player);*/
+        Player player = new Player();
+        controller.addNode(player);
         
         // BEGIN
         publisher.subscribe(network);
         container.start();
-        System.out.println("Juego iniciado");
-
-        container.getController().addNode(new Jugador());
-        //container.getController().addNode(new Cuadrado());
     }
 }
