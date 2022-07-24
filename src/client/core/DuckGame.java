c1532406e3a1ff70f501a494522a7f346fdbd58b
Feredev <hector1567xd@gmail.com>
2022-07-24 18:17:16 -0400

    Merge changes

--- src/client/core/DuckGame.java
+++ src/client/core/DuckGame.java
@@ -8,22 +8,28 @@ import client.game.engine.GameNetwork;
 import client.game.nodes.MapNode;
 import client.game.nodes.Player;
 import client.networking.Client;
-import common.networking.DuckPacketReader;
+import client.game.nodes.Camera;
+import client.game.nodes.Triangulito;
 import common.networking.engine.socket.SocketPublisher;
 
 public class DuckGame {
     public static void start(SocketPublisher publisher, Client client) {
         // APP GAME BUILDING
-        GameController controller = new GameController();
-        GameNetwork network = new GameNetwork(client);
-        GameContainer container = new GameContainer(Constants.SCALE, network, controller);
+            GameController controller = new GameController();
+            GameNetwork network = new GameNetwork(client);
+            GameContainer container = new GameContainer(Constants.SCALE, network, controller);
+
         // GAME
-        Player player = new Player();
-        MapNode mapa = new MapNode(container);
-        controller.addNode(mapa);
-        controller.addNode(player);
+            Player player = new Player();
+            MapNode mapa = new MapNode(container);
+            controller.addNode(mapa);
+            controller.addNode(player, "Player");
+            // LA CAMARA SIEMPRE DEBE IR DESPUES DEL JUGADOR
+            controller.setCamera(new Camera(container));
+            controller.addNode(new Triangulito());
+
         // BEGIN
-        publisher.subscribe(network);
-        container.start();
+            publisher.subscribe(network);
+            container.start();
     }
 }
