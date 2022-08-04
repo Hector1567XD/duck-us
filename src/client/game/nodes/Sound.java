183c1c7b889b117f91664180f0353b4c02a84041
Yisustxz <jesusoortiz2410@gmail.com>
2022-08-04 00:53:55 -0400

    ahora si

---
+++ src/client/game/nodes/Sound.java
@@ -0,0 +1,41 @@
+package client.game.nodes;
+
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
+public class Sound {
+   Clip clip;
+   URL soundURL[] = new URL[30]; 
+   
+   public Sound (){
+       soundURL[0] = getClass().getResource("/client/resources/game/sounds/Pisada 1.wav");
+       soundURL[1] = getClass().getResource("/client/resources/game/sounds/Pato impostor.wav");
+   }
+   
+   public void setFile(int i) {
+       try{
+         AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+         clip = AudioSystem.getClip();
+         clip.open(ais);
+       }catch(Exception e) {
+           
+       }
+           
+   }
+   
+   public void play(){
+       clip.start();
+   }
+   
+   public void loop() {
+       clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+   
+   public void stop() {
+       clip.stop();
+       
+   }
+}
